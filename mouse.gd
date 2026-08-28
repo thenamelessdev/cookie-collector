859a7a6be607caca31f6bb0f94bd114fd0c58149
Nameless <nameless@thenamelessdev.com>
2026-08-28 09:23:08 +0200

    feat(mouse): ability to toggle between the mouse modes

--- mouse.gd
+++ mouse.gd
@@ -5,4 +5,7 @@ func _ready() -> void:
 	
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
