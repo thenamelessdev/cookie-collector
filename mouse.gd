680b40d09a57c620f50b67bf91466b430b8e788b
Nameless <nameless@thenamelessdev.com>
2026-08-29 11:10:27 +0200

    feat: make esc return to menu and show mouse

--- mouse.gd
+++ mouse.gd
@@ -1,11 +1,5 @@
 extends Node
 
-func _ready() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
-		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		else:
-			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		get_tree().change_scene_to_file("res://menu.tscn")
